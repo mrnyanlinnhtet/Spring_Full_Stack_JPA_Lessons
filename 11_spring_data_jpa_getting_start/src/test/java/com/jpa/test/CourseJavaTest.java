3800efcbb36fb8f1359f988361c8c7c3ad38f488
MrNyanLinnHtet <mr.nyanlinnhtet13@gmail.com>
2023-02-13 09:49:19 +0630

    '08_data_jpa_start_00'

---
+++ 11_spring_data_jpa_getting_start/src/test/java/com/jpa/test/CourseJavaTest.java
@@ -0,0 +1,31 @@
+package com.jpa.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import com.jpa.entities.Course;
+import com.jpa.repositories.CourseRepositroy;
+
+@SpringJUnitConfig()
+public class CourseJavaTest {
+
+	@Autowired
+	private CourseRepositroy repo;
+
+	@ParameterizedTest
+	@CsvSource({ "Spring Online Course,6,400000,1" })
+	void course_save_test(String name, int duration, int fees, long id) {
+
+		var course = new Course(name, duration, fees);
+		var result = repo.save(course);
+
+		assertNotNull(course);
+		assertEquals(id, result.getId());
+	}
+
+}
